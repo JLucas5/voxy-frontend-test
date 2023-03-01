ee00e0413f841b0887c43050335803cd2881f1d8
João Lucas <joaolucaspachecoab@gmail.com>
2023-03-01 02:32:46 -0300

    Service mockup created

---
+++ src/app/services/people/people.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import people from '../people/people.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeopleService {
+
+  constructor() { }
+
+  getAllPeople(){
+    return people as object[]
+  }
+}
